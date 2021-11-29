f12674a323b20d0a860bf77608a7e57178906274
Nguyễn Văn Nhựt <85334608+PikaNguyen@users.noreply.github.com>
2021-11-29 18:20:03 +0700

    Add files via upload

---
+++ QuanLyThuVienCaNhan/QuanLyThuVienCaNhan/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuVienCaNhan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmiMuonSach_Click(object sender, EventArgs e)
+        {
+            frmMuonTra frmMuon = new frmMuonTra();
+            frmMuon.ShowDialog();
+        }
+
+        private void lsvDanhSach_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            frmSach frmSach = new frmSach();
+            frmSach.ShowDialog();
+        }
+
+        private void tsmThongKe_Click(object sender, EventArgs e)
+        {
+            frmThongKe frmThongKe = new frmThongKe();
+            frmThongKe.ShowDialog();
+        }
+
+        private void tsmThem_Click(object sender, EventArgs e)
+        {
+            frmSach frmSach = new frmSach();
+            frmSach.ShowDialog();
+        }
+    }
+}
